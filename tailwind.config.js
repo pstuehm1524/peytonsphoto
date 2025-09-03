0d26ea7aefaaa3ae1890c92861adb5697ee945d2
pbossev <peterbossev@gmail.com>
2025-09-02 23:25:38 -0400

    fixed mobile footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./src/**/*.{liquid,md}"],
-  theme: {},
+  theme: {
+    extend: {
+      height: {
+        svh: "100svh", // small viewport height
+        lvh: "100lvh", // large viewport height
+        dvh: "100dvh", // dynamic viewport height
+      },
+      minHeight: {
+        svh: "100svh",
+      },
+    },
+  },
   extend: {},
 }
