73f732b83da0783a99adcd103094b250af7e97f2
pbossev <peterbossev@gmail.com>
2025-09-03 00:01:52 -0400

    fixing footer again

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{liquid,md}"],
-  theme: {
-    extend: {
-      height: {
-        svh: "100svh", // small viewport height
-        lvh: "100lvh", // large viewport height
-        dvh: "100dvh", // dynamic viewport height
-      },
-      minHeight: {
-        svh: "100svh",
-      },
-    },
-  },
+  theme: {},
   extend: {},
 }
