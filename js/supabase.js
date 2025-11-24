e19216efb7ce0bd6ef59ede20c25b1cacd0fe38e
Peyton S <pstuehm@gmail.com>
2025-11-23 23:30:29 -0600

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,8 @@
 
-const SUPABASE_URL = "https://ygnfuztbwsngjihrinlr.supabase.co";
+const SUPABASE_URL = "https://mfojaoazhtoizjbcjbqu.supabase.co";
 
 const SUPABASE_ANON_KEY =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnbmZ1enRid3NuZ2ppaHJpbmxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5ODUwOTMsImV4cCI6MjA3ODU2MTA5M30.YlKPGDE3hfF9mOIVbUEQQFtN8gwt6L0MOYldH1AOMGw";
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mb2phb2F6aHRvaXpqYmNqYnF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5NDQ1NjIsImV4cCI6MjA3OTUyMDU2Mn0.ccHGMJlCgSHqgaz9Vd2cEvxUmjol9QgxnbbmzNbF6aw";
 
 console.log("Initializing Supabase client…");
 
